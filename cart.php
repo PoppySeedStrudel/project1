d778b8e537ebce83c26f3e280630ba9db27fd3ed
Cornelius <cornelius_baier@yahoo.de>
2012-05-18 16:42:18 +0200

    18.05.2012

--- cart.php
+++ cart.php
@@ -2,49 +2,24 @@
 
 session_start();
 
-for ($i = 0; $i < sizeof($_GET); ++$i) {
-    // echo "key: ".key($_GET)."<br>value: ".current($_GET)."<br>";
-    $meal = key($_GET);
-	$menge = current($_GET);
-	if ($menge != ""){
-		echo "You ordered " . $menge . " of product id " . $meal . ".<br>";
-		
-		$line = array($meal => $menge);
-		if (!isset($_SESSION['order'])) {
-			$_SESSION['order'] = array();
-			array_push($_SESSION['order'],$line);
-		// echo "Es klappt. Der initiale Array wurde angelegt.";
-		}
-		else {
-			array_push($_SESSION['order'],$line);
-			// echo $meal . " wurde eingefügt.<br>";
-		}
-	}
-    next($_GET);
- }
+
+$testarray = array("wert eins" => "erster wert", "wert zwei " => "zweiter wert");
 
 
-
-// $line = array("vorname" => $vorname, "nachname" => $nachname, "wert 1"=> $eins, "wert 2"=> $zwei, "wert 3" => $drei);
-
-// wenn die seite ohne den parameter vorname aufgerufen wird, wird ein leerer array angelegt.
-
-
-
-if (isset($_SESSION['order'])) {
-	foreach ($_SESSION['order'] as $meal => $menge):
-	//	foreach($bestellung as $zeile):
-			echo $meal . " - " . $menge . "<br>";
-	//	endforeach;
-	//	print_r($bestellung);
-	endforeach;
-foreach( $_SESSION['order'] as $name => $age){
-	echo "Name: $name, Age: $age <br />";
-}
+if (!isset($_SESSION['orders'])){
+	
+	$_SESSION['orders'] = $testarray;
+	
 }
 else {
-	echo "<br><a href=\"category.php\">link</a> <br>";
+	
+	echo "Session Cookie sitzt <br>";
+	print_r($_SESSION);
+	
 }
+
+echo "<br><a href=\"category.php\">link</a> <br>";
+
 
 ?>
 
@@ -54,6 +29,7 @@ else {
 
 
 <a href="destroy.php"><br> destroy</a>
+<?php  echo($_COOKIE['orders']);?>
 </body>
 
 </html>
--- destroy.php
+++ destroy.php
@@ -1,8 +1,10 @@
 <?php
 	session_start();
 	
-	unset($_SESSION['order']);
+	unset($_SESSION['product']);
+	unset($_SESSION['orders']);
 	
 	echo "<a href=\"cart.php\">back</a>";
 
 ?>
+
